f04da60f20db38034f2ac3e35a183a25336337de
Marco Pennacchiotti <mpennacchiotti@ebay.com>
2015-09-25 17:08:22 -0700

    add documentation and local refactor

--- src/main/java/com/penna/neural/common/Dataset.java
+++ src/main/java/com/penna/neural/common/Dataset.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Logger;
+
 import org.jblas.DoubleMatrix;
 
-public class Dataset{// implements Iterable<Instance> {
-
+public class Dataset{
+    
+    private static final Logger LOGGER = Logger.getLogger(MnistUtils.class.getName());
+    
     private List<Instance> instances;
 
     public Dataset() {
@@ -19,23 +23,41 @@ public class Dataset{// implements Iterable<Instance> {
     }
 
     /**
-     * Random constructor
+     * Constructs a dataset with random unlabelled instances.
+     * 
+     * @param numFeatures number of instances' features
+     * @param numInstances number of instances to the dataset
      */
-    public Dataset(int inputLayerSize, int outputLayerSize, int numInstances) {
+    public Dataset(int numFeatures, int numInstances) {
         instances = new ArrayList<Instance>();
         for (int i = 0; i < numInstances; i++) {
-            DoubleMatrix input = DoubleMatrix.randn(inputLayerSize, 1);
-            DoubleMatrix output = DoubleMatrix.randn(outputLayerSize, 1);
-            instances.add(new Instance(input, output));
+            DoubleMatrix features = DoubleMatrix.randn(numFeatures, 1);
+            instances.add(new Instance(features));
+        }
+    }
+    
+    /**
+     * Constructs a dataset with random labelled instances.
+     * 
+     * @param numFeatures number of instances' features
+     * @param numLabels number of instances'  labels
+     * @param numInstances number of instances to the dataset
+     */
+    public Dataset(int numFeatures, int numLabels, int numInstances) {
+        instances = new ArrayList<Instance>();
+        for (int i = 0; i < numInstances; i++) {
+            DoubleMatrix features = DoubleMatrix.randn(numFeatures, 1);
+            DoubleMatrix labels = DoubleMatrix.randn(numLabels, 1);
+            instances.add(new Instance(features, labels));
         }
     }
 
+    public Instance getInstance(int i) {
+        return instances.get(i);
+    }
+    
     public void add(Instance instance) {
         instances.add(instance);
-    }
-
-    public void shuffle() {
-        Collections.shuffle(instances);
     }
 
     public int size() {
@@ -50,11 +72,36 @@ public class Dataset{// implements Iterable<Instance> {
         return getInstances();
     }
 
+    /**
+     * Shuffle the order of the instances in the dataset.
+     */
+    public void shuffle() {
+        Collections.shuffle(instances);
+    }
+    
+    /**
+     * Get a subset of this dataset, by specifying the start and end index from which to subset
+     * from.
+     * 
+     * @param startIdx start index of the subset
+     * @param endIdx end index of the subset
+     * @return the subset dataset
+     */
     public Dataset getSubSet(int startIdx, int endIdx) {
         return new Dataset(instances.subList(startIdx, endIdx));
     }
-
-    public Instance getInstance(int i) {
-        return instances.get(i);
+    
+    /**
+     * Removed unlabelled instances from the dataset.
+     */
+    public void removeUnlabelledInstance() {
+        int numRemovedInstances = 0;
+        for  (int i = 0; i < instances.size(); i++) {
+            if (!instances.get(i).isLabelled()) {
+                instances.remove(i);
+                numRemovedInstances++;
+            }
+        }
+        LOGGER.info("Removed " + numRemovedInstances + " instances");
     }
 }
