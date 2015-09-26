58d4339ab15d06bd46cac655a0c86e04e07a0e3b
Marco Pennacchiotti <mpennacchiotti@ebay.com>
2015-09-25 17:09:19 -0700

    add exception definition

--- src/main/java/com/penna/neural/exceptions/NetworkInitializationException.java
+++ src/main/java/com/penna/neural/exceptions/NetworkInitializationException.java
@@ -3,6 +3,10 @@ package com.penna.neural.exceptions;
 @SuppressWarnings("serial")
 public class NetworkInitializationException extends Exception {
 
+    public NetworkInitializationException() {
+        super();
+    }
+    
     public NetworkInitializationException(String msg) {
         super(msg);
     }
